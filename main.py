a0cf24f0f47c3ec6d9e9c3f98edc08cfaba2decc
bonh <bonh@tcbs.com.vn>
2020-11-08 11:15:58 +0700

    draft for uploading and downloading

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Query
+from fastapi.responses import StreamingResponse
+import shutil
+from typing import List
 
+some_file_path = "hai.mp3"
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "xxHello World"}
+@app.get("/listen")
+async def listen(song: str = Query(...)):
+    file_like = open(song, mode="rb")
+    return StreamingResponse(file_like, media_type="video/mp4")
+
+
+@app.post("/image")
+async def image(images: List[UploadFile] = File(...)):
+    for image in images:
+        with open(image.filename, "wb") as buffer:
+            shutil.copyfileobj(image.file, buffer)
+    return {"filename": image.filename}
